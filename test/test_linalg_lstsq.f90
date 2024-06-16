c48c03ed80b42dfa6aaf723e39e2bf59951b742d
chuckyvt <138633930+chuckyvt@users.noreply.github.com>
2024-06-16 22:44:30 +0000

    Deploying to stdlib-fpm from @ chuckyvt/stdlib@87192e9113a2d42098f553377956d18db48f09ed 🚀

--- test/test_linalg_lstsq.f90
+++ test/test_linalg_lstsq.f90
@@ -2,7 +2,7 @@
 module test_linalg_least_squares
     use testdrive, only: error_type, check, new_unittest, unittest_type
     use stdlib_linalg_constants
-    use stdlib_linalg, only: lstsq
+    use stdlib_linalg, only: lstsq,solve_lstsq
     use stdlib_linalg_state, only: linalg_state_type
 
     implicit none (type,external)
@@ -18,6 +18,8 @@ module test_linalg_least_squares
         type(unittest_type), allocatable, intent(out) :: tests(:)
         
         allocate(tests(0))
+        
+        tests = [tests,new_unittest("issue_823",test_issue_823)]
 
         tests = [tests,new_unittest("least_squares_s",test_lstsq_one_s), &
                        new_unittest("least_squares_randm_s",test_lstsq_random_s)]
@@ -50,7 +52,7 @@ module test_linalg_least_squares
         call check(error,state%ok(),state%print())
         if (allocated(error)) return
         
-        call check(error, all(abs(p-ab)<1.0e-6_sp), 'data converged')
+        call check(error, all(abs(p-ab)<1.0e-4_sp), 'data converged')
         if (allocated(error)) return
         
         call check(error, rank==2, 'matrix rank == 2')
@@ -64,14 +66,18 @@ module test_linalg_least_squares
 
         type(linalg_state_type) :: state
         integer(ilp), parameter :: n = 12, m = 3
-        real(sp) :: xsol(m),x(m),y(n),A(n,m)
+        real :: Arnd(n,m),xrnd(m)
+        real(sp), allocatable :: x(:)
+        real(sp) :: xsol(m),y(n),A(n,m)
 
         ! Random coefficient matrix and solution
-        call random_number(A)
-        call random_number(xsol)
+        call random_number(Arnd)
+        call random_number(xrnd)
         
         ! Compute rhs
-        y = matmul(A,xsol)
+        A    = real(Arnd,sp)
+        xsol = real(xrnd,sp)
+        y    = matmul(A,xsol)
 
         ! Find polynomial
         x = lstsq(A,y,err=state)
@@ -79,7 +85,11 @@ module test_linalg_least_squares
         call check(error,state%ok(),state%print())
         if (allocated(error)) return
         
-        call check(error, all(abs(x-xsol)<1.0e-6_sp), 'data converged')
+        ! Check size
+        call check(error,size(x)==m)
+        if (allocated(error)) return
+        
+        call check(error, all(abs(x-xsol)<1.0e-4_sp), 'data converged')
         if (allocated(error)) return
         
     end subroutine test_lstsq_random_s    
@@ -108,7 +118,7 @@ module test_linalg_least_squares
         call check(error,state%ok(),state%print())
         if (allocated(error)) return
         
-        call check(error, all(abs(p-ab)<1.0e-6_dp), 'data converged')
+        call check(error, all(abs(p-ab)<1.0e-4_dp), 'data converged')
         if (allocated(error)) return
         
         call check(error, rank==2, 'matrix rank == 2')
@@ -122,14 +132,18 @@ module test_linalg_least_squares
 
         type(linalg_state_type) :: state
         integer(ilp), parameter :: n = 12, m = 3
-        real(dp) :: xsol(m),x(m),y(n),A(n,m)
+        real :: Arnd(n,m),xrnd(m)
+        real(dp), allocatable :: x(:)
+        real(dp) :: xsol(m),y(n),A(n,m)
 
         ! Random coefficient matrix and solution
-        call random_number(A)
-        call random_number(xsol)
+        call random_number(Arnd)
+        call random_number(xrnd)
         
         ! Compute rhs
-        y = matmul(A,xsol)
+        A    = real(Arnd,dp)
+        xsol = real(xrnd,dp)
+        y    = matmul(A,xsol)
 
         ! Find polynomial
         x = lstsq(A,y,err=state)
@@ -137,11 +151,55 @@ module test_linalg_least_squares
         call check(error,state%ok(),state%print())
         if (allocated(error)) return
         
-        call check(error, all(abs(x-xsol)<1.0e-6_dp), 'data converged')
+        ! Check size
+        call check(error,size(x)==m)
+        if (allocated(error)) return
+        
+        call check(error, all(abs(x-xsol)<1.0e-4_dp), 'data converged')
         if (allocated(error)) return
         
     end subroutine test_lstsq_random_d    
     
+    
+    ! Test issue #823
+    subroutine test_issue_823(error)
+        type(error_type), allocatable, intent(out) :: error
+        
+        ! Dimension of the problem.
+        integer(ilp), parameter :: n = 42
+        ! Data for the least-squares problem.
+        complex(dp) :: A(n+1, n), b(n+1), x_true(n), x_lstsq(n)
+        ! Internal variables.
+        real(dp), allocatable :: tmp(:, :, :), tmp_vec(:, :)
+        ! Error handler
+        type(linalg_state_type) :: state
+
+        ! Zero-out data.
+        A = 0.0_dp
+        b = 0.0_dp
+        x_lstsq = 0.0_dp
+        allocate(tmp(n+1, n, 2), tmp_vec(n, 2), source=0.0_dp)
+
+        ! Generate a random complex least-squares problem of size (n+1, n).
+        call random_number(tmp)
+        call random_number(tmp_vec)
+        
+        A      = cmplx(tmp(:, :, 1), tmp(:, :, 2), kind=dp)
+        x_true = cmplx(tmp_vec(:, 1), tmp_vec(:, 2), kind=dp)
+        b      = matmul(A, x_true)
+
+        ! Solve the lstsq problem.
+        call solve_lstsq(A, b, x_lstsq, err=state)
+          
+        ! Check that no segfault occurred
+        call check(error,state%ok(),'issue 823 returned '//state%print())
+        if (allocated(error)) return
+
+        ! Check that least squares are verified
+        call check(error,all(abs(x_true-x_lstsq)<sqrt(epsilon(0.0_dp))),'issue 823 results')
+        if (allocated(error)) return
+
+    end subroutine test_issue_823
 
 end module test_linalg_least_squares
 
