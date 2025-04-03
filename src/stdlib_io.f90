3989aa02f5970fee4ee49d5d21c7eacb3b09c62c
chuckyvt <138633930+chuckyvt@users.noreply.github.com>
2025-04-03 00:36:47 +0000

    Deploying to stdlib-fpm-ilp64 from @ chuckyvt/stdlib@b2e08d5300a641d144138035fa9687eeeb191661 🚀

--- src/stdlib_io.f90
+++ src/stdlib_io.f90
@@ -7,14 +7,36 @@ module stdlib_io
   use, intrinsic :: iso_fortran_env, only : input_unit
   use stdlib_kinds, only: sp, dp, xdp, qp, &
       int8, int16, int32, int64
-  use stdlib_error, only: error_stop
+  use stdlib_error, only: error_stop, state_type, STDLIB_IO_ERROR
   use stdlib_optval, only: optval
   use stdlib_ascii, only: is_blank
-  use stdlib_string_type, only : string_type
+  use stdlib_string_type, only : string_type, assignment(=), move
   implicit none
   private
   ! Public API
-  public :: loadtxt, savetxt, open, getline
+  public :: loadtxt, savetxt, open, get_line, get_file
+
+  !! version: experimental 
+  !!
+  !! Reads a whole ASCII file and loads its contents into a string variable. 
+  !! ([Specification](../page/specs/stdlib_io.html#get-file-read-a-whole-ascii-file-into-a-character-or-a-string-variable))
+  !! 
+  !!### Summary 
+  !! Subroutine interface for reading the content of a file into a string.
+  !!
+  !!### Description
+  !! 
+  !! This subroutine reads the entirety of a specified ASCII file and returns it as a string. The optional 
+  !! `err` argument allows for handling errors through the library's `state_type` class. 
+  !! An optional `logical` flag can be passed to delete the file after reading.  
+  !! 
+  !!@note Handles errors using the library's `state_type` error-handling class. If not provided, 
+  !! exceptions will trigger an `error stop`. 
+  !!         
+  interface get_file
+    module procedure :: get_file_char
+    module procedure :: get_file_string
+  end interface get_file
 
   ! Private API that is exposed so that we can test it in tests
   public :: parse_mode
@@ -49,12 +71,12 @@ module stdlib_io
   !> Version: experimental
   !>
   !> Read a whole line from a formatted unit into a string variable
-  interface getline
-    module procedure :: getline_char
-    module procedure :: getline_string
-    module procedure :: getline_input_char
-    module procedure :: getline_input_string
-  end interface getline
+  interface get_line
+    module procedure :: get_line_char
+    module procedure :: get_line_string
+    module procedure :: get_line_input_char
+    module procedure :: get_line_input_string
+  end interface get_line
 
   interface loadtxt
     !! version: experimental
@@ -151,6 +173,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -165,7 +188,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -176,7 +199,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -185,7 +208,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_rsp
     subroutine  loadtxt_rdp(filename, d, skiprows, max_rows, fmt)
@@ -251,6 +274,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -265,7 +289,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -276,7 +300,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -285,7 +309,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_rdp
     subroutine  loadtxt_iint8(filename, d, skiprows, max_rows, fmt)
@@ -351,6 +375,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -365,7 +390,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -376,7 +401,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -385,7 +410,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_iint8
     subroutine  loadtxt_iint16(filename, d, skiprows, max_rows, fmt)
@@ -451,6 +476,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -465,7 +491,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -476,7 +502,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -485,7 +511,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_iint16
     subroutine  loadtxt_iint32(filename, d, skiprows, max_rows, fmt)
@@ -551,6 +577,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -565,7 +592,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -576,7 +603,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -585,7 +612,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_iint32
     subroutine  loadtxt_iint64(filename, d, skiprows, max_rows, fmt)
@@ -651,6 +678,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -665,7 +693,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -676,7 +704,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -685,7 +713,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_iint64
     subroutine  loadtxt_csp(filename, d, skiprows, max_rows, fmt)
@@ -752,6 +780,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -766,7 +795,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -777,7 +806,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -786,7 +815,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_csp
     subroutine  loadtxt_cdp(filename, d, skiprows, max_rows, fmt)
@@ -853,6 +882,7 @@ contains
         
         if (ios/=0) then 
            write(msgout,1) trim(iomsg),i,trim(filename) 
+           1 format('loadtxt: error <',a,'> skipping line ',i0,' of ',a,'.')
            call error_stop(msg=trim(msgout))
         end if
         
@@ -867,7 +897,7 @@ contains
           read (s,*,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if          
           
@@ -878,7 +908,7 @@ contains
           read (s,fmt_,iostat=ios,iomsg=iomsg) d(i, :)
           
           if (ios/=0) then 
-             write(msgout,1) trim(iomsg),size(d,2),i,trim(filename) 
+             write(msgout,2) trim(iomsg),size(d,2),i,trim(filename)
              call error_stop(msg=trim(msgout))
           end if             
           
@@ -887,7 +917,7 @@ contains
 
       close(s)
       
-      1 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
+      2 format('loadtxt: error <',a,'> reading ',i0,' values from line ',i0,' of ',a,'.')
 
     end subroutine loadtxt_cdp
 
@@ -1220,7 +1250,7 @@ contains
     number_of_columns = 0
     
     ! Read first non-skipped line as a whole
-    call getline(s, line, ios)
+    call get_line(s, line, ios)
     if (ios/=0 .or. .not.allocated(line)) return
 
     lastblank = .true.
@@ -1392,7 +1422,7 @@ contains
   !> Version: experimental
   !>
   !> Read a whole line from a formatted unit into a deferred length character variable
-  subroutine getline_char(unit, line, iostat, iomsg)
+  subroutine get_line_char(unit, line, iostat, iomsg)
     !> Formatted IO unit
     integer, intent(in) :: unit
     !> Line to read
@@ -1434,12 +1464,12 @@ contains
     else if (stat /= 0) then
       call error_stop(trim(msg))
     end if
-  end subroutine getline_char
+  end subroutine get_line_char
 
   !> Version: experimental
   !>
   !> Read a whole line from a formatted unit into a string variable
-  subroutine getline_string(unit, line, iostat, iomsg)
+  subroutine get_line_string(unit, line, iostat, iomsg)
     !> Formatted IO unit
     integer, intent(in) :: unit
     !> Line to read
@@ -1451,14 +1481,14 @@ contains
 
     character(len=:), allocatable :: buffer
 
-    call getline(unit, buffer, iostat, iomsg)
+    call get_line(unit, buffer, iostat, iomsg)
     line = string_type(buffer)
-  end subroutine getline_string
+  end subroutine get_line_string
 
   !> Version: experimental
   !>
   !> Read a whole line from the standard input into a deferred length character variable
-  subroutine getline_input_char(line, iostat, iomsg)
+  subroutine get_line_input_char(line, iostat, iomsg)
     !> Line to read
     character(len=:), allocatable, intent(out) :: line
     !> Status of operation
@@ -1466,13 +1496,13 @@ contains
     !> Error message
     character(len=:), allocatable, optional :: iomsg
 
-    call getline(input_unit, line, iostat, iomsg)
-  end subroutine getline_input_char
+    call get_line(input_unit, line, iostat, iomsg)
+  end subroutine get_line_input_char
 
   !> Version: experimental
   !>
   !> Read a whole line from the standard input into a string variable
-  subroutine getline_input_string(line, iostat, iomsg)
+  subroutine get_line_input_string(line, iostat, iomsg)
     !> Line to read
     type(string_type), intent(out) :: line
     !> Status of operation
@@ -1480,7 +1510,122 @@ contains
     !> Error message
     character(len=:), allocatable, optional :: iomsg
 
-    call getline(input_unit, line, iostat, iomsg)
-  end subroutine getline_input_string
+    call get_line(input_unit, line, iostat, iomsg)
+  end subroutine get_line_input_string
+
+  !> Version: experimental
+  !> 
+  !> Reads a whole ASCII file and loads its contents into a string variable.
+  !> The function handles error states and optionally deletes the file after reading.
+  subroutine get_file_string(filename,file,err,delete) 
+      !> Input file name
+      character(*), intent(in) :: filename
+      !> Output string variable
+      type(string_type), intent(out) :: file
+      !> [optional] State return flag. On error, if not requested, the code will stop.
+      type(state_type), optional, intent(out) :: err
+      !> [optional] Delete file after reading? Default: do not delete
+      logical, optional, intent(in) :: delete
+        
+      ! Local variables
+      character(len=:), allocatable :: filestring
+      
+      ! Process output
+      call get_file_char(filename,filestring,err,delete)
+      call move(from=fileString,to=file)
+
+  end subroutine get_file_string
+
+  !> Version: experimental
+  !> 
+  !> Reads a whole ASCII file and loads its contents into an allocatable `character` variable.
+  !> The function handles error states and optionally deletes the file after reading.
+  subroutine get_file_char(filename,file,err,delete) 
+      !> Input file name
+      character(*), intent(in) :: filename
+      !> Output string variable
+      character(len=:), allocatable, intent(out) :: file
+      !> [optional] State return flag. On error, if not requested, the code will stop.
+      type(state_type), optional, intent(out) :: err
+      !> [optional] Delete file after reading? Default: do not delete
+      logical, optional, intent(in) :: delete
+        
+      ! Local variables
+      type(state_type) :: err0
+      character(len=512) :: iomsg
+      integer :: lun,iostat
+      integer(int64) :: errpos,file_size
+      logical :: is_present,want_deleted
+
+      !> Check if the file should be deleted after reading
+      if (present(delete)) then 
+         want_deleted = delete
+      else
+         want_deleted = .false.   
+      end if
+
+      !> Check file existing
+      inquire(file=filename, exist=is_present)
+      if (.not.is_present) then
+         allocate(character(len=0) :: file)
+         err0 = state_type('get_file',STDLIB_IO_ERROR,'File not present:',filename)
+         call err0%handle(err)
+         return
+      end if
+      
+      !> Retrieve file size
+      inquire(file=filename,size=file_size)
+      
+      invalid_size: if (file_size<0) then 
+
+          allocate(character(len=0) :: file)
+          err0 = state_type('get_file',STDLIB_IO_ERROR,filename,'has invalid size=',file_size)
+          call err0%handle(err)
+          return            
+            
+      endif invalid_size  
+            
+      ! Read file
+      open(newunit=lun,file=filename, &
+           form='unformatted',action='read',access='stream',status='old', &
+           iostat=iostat,iomsg=iomsg)
+             
+      if (iostat/=0) then 
+         allocate(character(len=0) :: file)
+         err0 = state_type('get_file',STDLIB_IO_ERROR,'Cannot open',filename,'for read:',iomsg)
+         call err0%handle(err)
+         return
+      end if     
+        
+      allocate(character(len=file_size) :: file)
+        
+      read_data: if (file_size>0) then 
+            
+          read(lun, pos=1, iostat=iostat, iomsg=iomsg) file
+            
+          ! Read error
+          if (iostat/=0) then 
+                
+              inquire(unit=lun,pos=errpos)                    
+              err0 = state_type('get_file',STDLIB_IO_ERROR,iomsg,'(',filename,'at byte',errpos,')')
+              call err0%handle(err)
+              return
+
+          endif
+            
+      end if read_data
+                   
+      if (want_deleted) then 
+         close(lun,iostat=iostat,status='delete')
+         if (iostat/=0) err0 = state_type('get_file',STDLIB_IO_ERROR,'Cannot delete',filename,'after reading')
+      else
+         close(lun,iostat=iostat)
+         if (iostat/=0) err0 = state_type('get_file',STDLIB_IO_ERROR,'Cannot close',filename,'after reading')
+      endif 
+      
+      ! Process output
+      call err0%handle(err)
+
+  end subroutine get_file_char
 
 end module stdlib_io
